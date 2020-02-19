4de4d20d63983257045467c384fa27a4f2a4b6b2
sesiliafenina <fenina.gunawan2605@gmail.com>
2020-02-19 15:53:12 +0800

    added player movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject moveJoystick;
+    public float speed = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
-        // this.gameobject.GetComponent<Weapon>();
+        Joystick joystick = moveJoystick.GetComponent<Joystick>();
+        joystick.joystickMovedEvent += updatePlayerPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void updatePlayerPosition(float angle)
+    {
+        Vector3 position = this.transform.position;
+        Vector3 delta = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+        this.transform.position += delta * Time.deltaTime;
     }
 }
