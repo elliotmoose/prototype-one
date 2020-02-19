a23cffa1d018f647223e05d30b98d8a0a3305de9
sesiliafenina <fenina.gunawan2605@gmail.com>
2020-02-19 16:13:06 +0800

    added player movements

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float dnaAmount = 0;
+    public void AddDna(float amount) {
+        dnaAmount += amount;
+    }
+
+    public GameObject moveJoystick;
+    public GameObject attackJoystick;
+    public float speed = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Joystick joystickMove = moveJoystick.GetComponent<Joystick>();
+        Joystick joystickAttack = attackJoystick.GetComponent<Joystick>();
+        joystickMove.joystickMovedEvent += UpdatePlayerPosition;
+        joystickAttack.joystickMovedEvent += RotatePlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void UpdatePlayerPosition(float angle)
+    {
+        Vector3 position = this.transform.position;
+        Quaternion rotation = this.transform.rotation;
+        Vector3 delta = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward;
+        this.transform.position += delta * Time.deltaTime * speed;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+    }
+
+    private void RotatePlayer(float angle)
+    {
+        Quaternion rotation = this.transform.rotation;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+    }
+}
