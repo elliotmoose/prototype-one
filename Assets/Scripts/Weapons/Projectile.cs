a0aa24f1d5e66f56d3287fe1e5e085d97efc30d2
T.Viet <victorpham1997@gmail.com>
2020-02-23 19:27:27 +0800

    Added Weapons and Projectile and their basic behavior

---
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col){
+    	if(col.gameObject.tag == "enemy"){
+    		 Debug.Log("Hit enemy");
+	    	Destroy(gameObject);    		
+    	}
+    	else if(col.gameObject.tag == "enemy"){
+    		 Debug.Log("Hit enemy");
+	    	Destroy(gameObject);    		
+    	}
+    	// Debug.Log("Bullet gone due to hitting: " + col.gameObject.tag);
+    }
+    void OnBecameInvisible() {
+         Destroy(gameObject);
+    }
+}
