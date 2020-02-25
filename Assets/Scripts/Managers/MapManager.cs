9dc15cab5b9922e47d06e9adb90d5a9c6e28aa87
Elliot Koh <appdev.moose@gmail.com>
2020-02-25 11:38:45 +0800

    Ensure enemies don't spawn outside of the map

---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{   
+    public float mapSize = 10; //a map of size 10 has a width of size 100units
+
+    private GameObject _map;
+    public static MapManager GetInstance() 
+    {
+        GameObject gameManager = GameObject.Find("GameManager");
+        if(gameManager == null) 
+        {
+            Debug.LogError("GameManager has not been instantiated yet");
+            return null;
+        }
+
+        MapManager mapManager = gameManager.GetComponent<MapManager>();
+
+        if(mapManager == null) 
+        {
+            Debug.LogError("GameManager has no component MapManager");
+            return null;
+        }
+
+        return mapManager;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _map = GameObject.Find("Map");
+
+        if(_map != null) 
+        {
+            _map.transform.localScale = new Vector3(mapSize, mapSize, mapSize);
+        }
+        else 
+        {
+            Debug.LogWarning("No map object with name \"Map\"");
+        }
+    }
+
+    public static bool IsInMap(Vector3 position) {
+        MapManager mapManager = GetInstance();
+        Vector3 mapCenter = mapManager._map.transform.position;
+        float mapSize = mapManager.mapSize;
+        bool isXBounded = Mathf.Abs(position.x - mapCenter.x) < (mapSize*10)/2;
+        bool isZBounded = Mathf.Abs(position.z - mapCenter.z) < (mapSize*10)/2;
+        return isXBounded && isZBounded;
+    }
+}
