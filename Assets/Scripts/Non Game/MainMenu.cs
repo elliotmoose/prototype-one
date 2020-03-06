28fa6c4a932b45493583f18341cb3aa6a0a9e9be
Glenda <glendaqihuiwee@gmail.com>
2020-03-07 03:12:01 +0800

    Added pause, resume and quit function

--- Assets/Scripts/Non Game/MainMenu.cs
+++ Assets/Scripts/Non Game/MainMenu.cs
@@ -5,11 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
+
     public void PlayGame(){
         SceneManager.LoadScene("Main");
     }
 
-    public void LoadSettings(){
-        
+    public void QuitGame(){
+        SceneManager.LoadScene("Start");
     }
+
+    bool isPaused = false;
+    public void PauseResumeGame(){
+        if (isPaused){
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+        else{
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+
+    
 }
