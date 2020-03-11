34b84b189052ad759c50a3bd113fe1ea56828986
Elliot Koh <appdev.moose@gmail.com>
2020-03-11 13:12:00 +0800

    Moved tab selection to code

--- Assets/Scripts/UI/ShopMenu.cs
+++ Assets/Scripts/UI/ShopMenu.cs
@@ -5,6 +5,15 @@ using UnityEngine.UI;
 
 public class ShopMenu : MonoBehaviour
 {
+    //tabs
+    public GameObject equippedTab;
+    public GameObject buyWeaponTab;
+    public GameObject upgradePlayerTab;
+    public Text equippedTabButtonText;
+    public Text buyWeaponTabButtonText;
+    public Text upgradePlayerTabButtonText;
+
+    //buy weapon
     public GameObject weaponsScrollViewContentPanel;
     public GameObject weaponButtonPrefab;
     public Button buyWeaponButton;
@@ -24,8 +33,57 @@ public class ShopMenu : MonoBehaviour
         shop = Shop.GetInstance();
         buyWeaponButton.onClick.AddListener(BuyWeapon);
         UpdateWeaponsScrollView();
+        SelectTab(0);
     }
 
+    #region Tab Selector 
+    public void SelectTab(int index) 
+    {
+        Color tabSelectedColor;
+        Color tabDeselectedColor;
+        ColorUtility.TryParseHtmlString("#F24545", out tabSelectedColor);
+        ColorUtility.TryParseHtmlString("#717171", out tabDeselectedColor);
+
+        switch(index) 
+        {
+            //equipped
+            case 0:
+                equippedTab.SetActive(true);
+                buyWeaponTab.SetActive(false);
+                upgradePlayerTab.SetActive(false);
+
+                equippedTabButtonText.color = tabSelectedColor;
+                buyWeaponTabButtonText.color = tabDeselectedColor;
+                upgradePlayerTabButtonText.color = tabDeselectedColor;
+                break;
+            
+            //buy
+            case 1:
+                equippedTab.SetActive(false);
+                buyWeaponTab.SetActive(true);
+                upgradePlayerTab.SetActive(false);
+
+                equippedTabButtonText.color = tabDeselectedColor;
+                buyWeaponTabButtonText.color = tabSelectedColor;
+                upgradePlayerTabButtonText.color = tabDeselectedColor;
+                break;
+            case 2:
+                equippedTab.SetActive(false);
+                buyWeaponTab.SetActive(false);
+                upgradePlayerTab.SetActive(true);            
+
+                equippedTabButtonText.color = tabDeselectedColor;
+                buyWeaponTabButtonText.color = tabDeselectedColor;
+                upgradePlayerTabButtonText.color = tabSelectedColor;
+                break;
+
+            default: 
+                break;
+        }
+    }
+    #endregion
+
+    #region Buy Weapons Tab
     public void UpdateWeaponsScrollView() 
     {
         foreach (Transform child in weaponsScrollViewContentPanel.transform) {
@@ -79,4 +137,6 @@ public class ShopMenu : MonoBehaviour
 
         shop.PurchaseWeapon(selectedWeapon);
     }
+
+    #endregion
 }
