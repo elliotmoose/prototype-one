be73c4df12effe4d149452ba112d1d2f7c0ad2aa
T.Viet <victorpham1997@gmail.com>
2020-03-25 10:46:30 +0800

    Added split for normal gun

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -33,8 +33,8 @@ public class Player : Entity
         SetMovementSpeed(3.5f);
         
         //set 
-        activeWeapons[0] = WeaponData.FlameThrowerWeaponData();
-        // activeWeapons[0] = WeaponData.StandardWeaponData();
+        // activeWeapons[0] = WeaponData.FlameThrowerWeaponData();
+        activeWeapons[0] = WeaponData.StandardWeaponData();
         EquipWeapon(activeWeapons[0]); //equip first weapon
                 
         // EntityEffect slowEffect = new SlowEffect(this);
--- Assets/Scripts/Weapons/GunWeapon.cs
+++ Assets/Scripts/Weapons/GunWeapon.cs
@@ -7,19 +7,111 @@ public class GunWeapon : Weapon
 
     public GameObject projectile;
     public Transform projectileSpawnPoint;
-
+    private float _deflectedAngle;
+    public GameObject projectileObj;
+    public Projectile projectileScript;
+    public Rigidbody projectileRB;
     protected override void Fire() {
 
         float bulletNumber =  this._weaponData.GetAttackPropertyValue("BULLET_SPLIT");
 
+        if(bulletNumber == 1){
+            _deflectedAngle = 0f;
+            
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
 
-        GameObject projectileObj = 	GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;	
-			
-        Projectile projectileScript = projectileObj.GetComponent<Projectile>();  
-        projectileScript.Activate(this._weaponData, this._owner);
-        projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new Vector3(_deflectedAngle, 0,0));
+        }
 
-        Rigidbody projectileRB = projectileObj.GetComponent<Rigidbody>();
-        projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new Vector3(0,3,0));
+        else if(bulletNumber == 2){
+            _deflectedAngle = 5f;
+            
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new Vector3(_deflectedAngle, 0,0));
+
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new Vector3( -1f* _deflectedAngle, 0,0));
+            
+        }
+
+        else if(bulletNumber == 3){
+            _deflectedAngle = 10f;
+            
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new Vector3(-1f*_deflectedAngle, 0,0));
+
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new  Vector3( _deflectedAngle, 0,0));
+
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new  Vector3( 0, 0,0));
+        }
+
+        else if(bulletNumber == 4){
+            _deflectedAngle = 5f;
+            
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new  Vector3( -2f*_deflectedAngle, 0,0));
+
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new  Vector3( -1f*2.75f, 0,0));
+
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new  Vector3(2f*_deflectedAngle, 0,0));
+
+            projectileObj =     GameObject.Instantiate(projectile, projectileSpawnPoint.transform.position,projectileSpawnPoint.transform.rotation) as GameObject;  
+            projectileScript = projectileObj.GetComponent<Projectile>();  
+            projectileScript.Activate(this._weaponData, this._owner);
+            projectileScript.SetOrigin(projectileSpawnPoint.transform.position);
+
+            projectileRB = projectileObj.GetComponent<Rigidbody>();
+            projectileRB.velocity = projectileSpawnPoint.TransformDirection(Vector3.forward*20 + new  Vector3( 2.75f, 0,0));
+        }
+
+       
     }
 }
