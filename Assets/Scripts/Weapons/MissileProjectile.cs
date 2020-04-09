94eea655696c4a024af0ac923390e3d8b0a3f402
ImForcedToDownloadThis <fenina.gunawan2605@gmail.com>
2020-04-09 17:16:33 +0800

    added missile projectile vfx

--- Assets/Scripts/Weapons/MissileProjectile.cs
+++ Assets/Scripts/Weapons/MissileProjectile.cs
@@ -8,9 +8,36 @@ public class MissileProjectile : MonoBehaviour
     private Entity _owner;
     private Vector3 _sourcePosition;
     private Vector3 _targetPosition;
+    public GameObject muzzlePrefab;
+    public GameObject hitPrefab;
+    public AudioClip shotSFX;
+    public AudioClip hitSFX; 
+    public List<GameObject> trails;
     private float _flightDuration = 1.45f;
     private float _timeAccelerationFactor = 1.7f;
     private float _timeElapsed = 0;
+
+    private void Start()
+    {
+        if (muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, Quaternion.identity);
+            muzzleVFX.transform.forward = gameObject.transform.forward;
+            var ps = muzzleVFX.GetComponent<ParticleSystem>();
+            if (ps != null)
+                Destroy(muzzleVFX, ps.main.duration);
+            else
+            {
+                var psChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleVFX, psChild.main.duration);
+            }
+        }
+
+        if (shotSFX != null && GetComponent<AudioSource>())
+        {
+            GetComponent<AudioSource>().PlayOneShot(shotSFX);
+        }
+    }
 
     void Update()
     {
@@ -84,7 +111,66 @@ public class MissileProjectile : MonoBehaviour
         Entity entity = col.gameObject.GetComponent<Entity>();
         if (_owner.IsOppositeTeam(entity))
         {
+            if (shotSFX != null && GetComponent<AudioSource>())
+            {
+                GetComponent<AudioSource>().PlayOneShot(hitSFX);
+            }
+
+            if (trails.Count > 0)
+            {
+                for (int i = 0; i < trails.Count; i++)
+                {
+                    trails[i].transform.parent = null;
+                    var ps = trails[i].GetComponent<ParticleSystem>();
+                    if (ps != null)
+                    {
+                        ps.Stop();
+                        Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                    }
+                }
+            }
+            if (hitPrefab != null)
+            {
+                var hitVFX = Instantiate(hitPrefab, col.transform.position, col.transform.rotation);
+                var ps = hitVFX.GetComponent<ParticleSystem>();
+                if (ps == null)
+                {
+                    var psChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                    Destroy(hitVFX, psChild.main.duration);
+                }
+                else
+                    Destroy(hitVFX, ps.main.duration);
+            }
+
+            StartCoroutine(DestroyParticle(0f));
             Explode();
         }
+    }
+
+    public IEnumerator DestroyParticle(float waitTime)
+    {
+
+        if (transform.childCount > 0 && waitTime != 0)
+        {
+            List<Transform> tList = new List<Transform>();
+
+            foreach (Transform t in transform.GetChild(0).transform)
+            {
+                tList.Add(t);
+            }
+
+            while (transform.GetChild(0).localScale.x > 0)
+            {
+                yield return new WaitForSeconds(0.01f);
+                transform.GetChild(0).localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+                for (int i = 0; i < tList.Count; i++)
+                {
+                    tList[i].localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+                }
+            }
+        }
+
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
     }
 }
